52d90851c31008c94cb98f6381a1f4db9f9162c0
YusiZhang <yusi.leo.zhang@gmail.com>
2014-01-19 22:08:13 -0500

    some methods

--- MutualFund/src/model/CustomerDAO.java
+++ MutualFund/src/model/CustomerDAO.java
@@ -8,6 +8,9 @@ package model;
 import org.genericdao.ConnectionPool;
 import org.genericdao.DAOException;
 import org.genericdao.GenericDAO;
+import org.genericdao.MatchArg;
+import org.genericdao.RollbackException;
+import org.genericdao.Transaction;
 
 import databeans.CustomerBean;
 
@@ -18,5 +21,94 @@ public class CustomerDAO extends GenericDAO<CustomerBean>{
 	public CustomerDAO(String tableName, ConnectionPool cp) throws DAOException {
 		super(CustomerBean.class, tableName, cp);
 	}
-
+	
+	
+	/*
+	 * Get customer id
+	 * @param username
+	 * @return userid or -1 if the username not existed.
+	 */
+	
+	public int getCustomerId(String username) throws RollbackException{
+		
+			CustomerBean [] dbCustomer = match(MatchArg.equals("username", username));
+			if(dbCustomer.length>0) return dbCustomer[0].getCustomer_id();
+			else return -1;
+	}
+	
+	/*
+	 * Change password
+	 * @param customer_id, password
+	 * @return void
+	 */
+	public void changePassword(int customer_id, String password) throws RollbackException{
+		try{
+			Transaction.begin();
+			CustomerBean dbCustomer = read(customer_id);
+			
+			if(dbCustomer==null) {
+				throw new RollbackException("User "+dbCustomer.getUsername()+" no longer exists");
+			}
+			
+			dbCustomer.setPassword(password);
+			
+			update(dbCustomer);
+			Transaction.commit();
+		}finally {
+			if(Transaction.isActive()) Transaction.rollback();
+		}
+	}
+	
+	/*
+	 * Check Password
+	 * @param customer_id, password
+	 * @return true if the password is correct otherwise false
+	 */
+	public boolean checkPassword(int customer_id, String password) throws RollbackException{
+		
+			CustomerBean db = read(customer_id);
+			if(db==null) {
+//				throw new RollbackException("User "+db.getUsername()+" no longer exists");
+				return false;
+			}
+			if(db.getPassword().equals(password)) return true;
+			else return false;
+		
+	}
+	
+	/*
+	 * Get all information of a certain customer
+	 * @param customer id
+	 * @return CustomerBean or null if customer_id not exist
+	 */
+	public CustomerBean getCustomerInfo(int customer_id) throws RollbackException{
+		CustomerBean db = read(customer_id);
+		if(db==null) return null;
+		return db;
+	}
+	
+	/*
+	 * Update cash
+	 * @param customer_id, cash(long)
+	 * @return void
+	 */
+	public void updataCash(int customer_id,long cash) throws RollbackException{
+		try{
+			Transaction.begin();
+			CustomerBean dbCustomer = read(customer_id);
+			
+			if(dbCustomer==null) {
+				throw new RollbackException("User "+dbCustomer.getUsername()+" no longer exists");
+			}
+			
+			dbCustomer.setCash(cash);
+			
+			update(dbCustomer);
+			Transaction.commit();
+		}finally {
+			if(Transaction.isActive()) Transaction.rollback();
+		}
+	}
+	
+	
 }
