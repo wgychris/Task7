f19e85b7cf9d2dddd30e6b45ed9d37a1452ff3d5
camelliayang <camelliadata@gmail.com>
2014-01-21 21:19:12 -0500

    1

--- MutualFund/src/formbeans/ChangePwdForm.java
+++ MutualFund/src/formbeans/ChangePwdForm.java
@@ -6,34 +6,56 @@ import java.util.List;
 import org.mybeans.form.FormBean;
 
 public class ChangePwdForm extends FormBean {
-	private String confirmPassword;
+	private String oldPassword;
 	private String newPassword;
-	
-	public String getConfirmPassword() { return confirmPassword; }
-	public String getNewPassword()     { return newPassword;     }
-	
-	public void setConfirmPassword(String s) { confirmPassword = s.trim(); }
-	public void setNewPassword(String s)     { newPassword     = s.trim(); }
+	private String confirmPassword;
 
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+	public void setOldPassword(String s) {
+		oldPassword = s.trim();
+	}
+	public void setConfirmPassword(String s) {
+		confirmPassword = s.trim();
+	}
+
+	public void setNewPassword(String s) {
+		newPassword = s.trim();
+	}
+	
 	public List<String> getValidationErrors() {
 		List<String> errors = new ArrayList<String>();
 
 		if (newPassword == null || newPassword.length() == 0) {
 			errors.add("New Password is required");
 		}
-		
+
 		if (confirmPassword == null || confirmPassword.length() == 0) {
 			errors.add("Confirm Pwd is required");
 		}
 		
+		if (oldPassword == null || oldPassword.length() == 0) {
+			errors.add("Old Pwd is required");
+		}
 		if (errors.size() > 0) {
 			return errors;
 		}
-		
+
 		if (!newPassword.equals(confirmPassword)) {
 			errors.add("Passwords do not match");
 		}
 
 		return errors;
 	}
+
+	
 }
